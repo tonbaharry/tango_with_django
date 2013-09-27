5c375511e984f4f7f55626d88b79a5c8b4215171
David Maxwell <maxwelld90@gmail.com>
2013-09-27 15:23:42 +0100

    Working through Models chapter code

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField()
+	likes = models.IntegerField()
+	
+	def __unicode__(self):
+		return self.name
+
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField()
+	
+	def __unicode__(self):
+		return self.title
