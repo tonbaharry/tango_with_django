23a328d86c4247adcac9947110ea1a214bd5ab8d
David Maxwell <maxwelld90@gmail.com>
2013-09-27 14:38:15 +0100

    First version of Chapter 4 code.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,19 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>")
+	# Request the context of the request.
+	context = RequestContext(request)
+	
+	# Construct a dictionary to pass to the template engine as its context.
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	
+	# Return a rendered response to send to the client.
+	# Note that the first parameter is the path to te template we wish to use.
+	return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-	return HttpResponse("Rango says: Here is the about page. <a href='/rango/'>Index</a>")
+	context = RequestContext(request)
+	
+	return render_to_response('rango/about.html', None, context)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -1,5 +1,16 @@
 # Django settings for tango_with_django_project project.
 
+# Import the OS module and work out our project's paths
+import os
+PROJECT_PATH = os.getcwd()
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+
+# Printing paths for sanity's sake
+print "Project root: ", PROJECT_PATH
+print "Templates: ", TEMPLATE_PATH
+print "Static: ", STATIC_PATH
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -50,12 +61,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -72,6 +83,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +123,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
