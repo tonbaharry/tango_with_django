8becba4010ad9f486e1d55ae5854310de21067d0
Can Ibanoglu <can@howyoucod.in>
2013-10-22 09:30:32 +0300

    Changed fields attribute to use tuples instead of lists in rango/forms.py, deleted the context variable from the user_logout view as it was not used

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -45,7 +45,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ('username', 'email', 'password')
 
 class UserProfileForm(forms.ModelForm):
 
@@ -54,4 +54,4 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        fields = ['website', 'picture']
+        fields = ('website', 'picture')
